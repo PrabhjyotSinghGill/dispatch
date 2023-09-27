b955874ed86c0554cea8eb6d2ef2faab2cadeb26
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-09-26 21:13:31 -0400

    App : components header+footer added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <div className="app">
-    {/* header */}
-    {/* body */}
-    {/* footer */}
-  </div>;
+  return (
+    <div className="app">
+      {/* header */}
+      <Header></Header>
+      {/* body */}
+      {/* footer */}
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
